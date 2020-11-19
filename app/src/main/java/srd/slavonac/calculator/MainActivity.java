f4f6d8fa65487e1779579b28137dd1a5b328a2e7
Dominik277 <dominikdoric277@gmail.com>
2020-11-19 16:49:05 +0100

    Vizualni dio zavrsen te dodane seOnClickListener metode.

--- app/src/main/java/srd/slavonac/calculator/MainActivity.java
+++ app/src/main/java/srd/slavonac/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package srd.slavonac.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     Button buttonNula,buttonJedan,buttonDva,buttonTri,buttonCetiri,
             buttonPet,buttonSedam,buttonSest,buttonOsam,buttonDevet,
             buttonPlus,buttonPlusMinus,buttonPuta,buttonPodijeljeno,
-            buttonMinus,buttonObrisi,buttonOdgovor;
+            buttonMinus,buttonObrisi,buttonOdgovor,buttonTocka;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,121 @@ public class MainActivity extends AppCompatActivity {
         buttonMinus = findViewById(R.id.buttonMinus);
         buttonObrisi = findViewById(R.id.buttonObrisi);
         buttonOdgovor = findViewById(R.id.buttonOdgovor);
+        buttonTocka = findViewById(R.id.buttonTocka);
+
+        buttonNula.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonJedan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonDva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonTri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonCetiri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonPet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonSest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonSedam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonOsam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonDevet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonPuta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonPodijeljeno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonObrisi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonOdgovor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonPlusMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
